453181c6473b0d858b440b2d5109fc6e48ab1a29
syed <user.email>
2022-07-25 10:33:37 +0100

    added baby names file and sorted alpheb

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import { greet } from "./utils/greet";
+import babyNames from './baby-names.json' 
+
+
+babyNames.sort(function(a, b) {
+  const textA = a.name.toUpperCase();
+  const textB = b.name.toUpperCase();
+return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+})
+
+for (let i = 0; i < 10; i++) {    
+  console.log(babyNames[i].name)
+}
+
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+    <></>
+  )
 }
 
 export default App;
