786d1455b84325483c45acb2211a77a63cc0ca20
syed <user.email>
2022-07-26 12:45:45 +0100

    pretty

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import NamesBlock from "./Components/namesBlock";
 
-
 function App(): JSX.Element {
   return (
-    <main style={{border: '5px green solid', width:'80%', height:'300px', display:'flex',
-     flexDirection:'row', alignItems:'start', justifyContent:'space-evenly',marginLeft:'auto', marginRight:'auto'}}>
-      <NamesBlock
-        {...['hey','str']}
-      />
+    <main
+      style={{
+        border: "5px green solid",
+        width: "80%",
+        height: "300px",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "start",
+        justifyContent: "space-evenly",
+        marginLeft: "auto",
+        marginRight: "auto",
+      }}
+    >
+      <NamesBlock {...["hey", "str"]} />
     </main>
-  )
+  );
 }
 
 export default App;
--- src/Components/namesBlock.tsx
+++ src/Components/namesBlock.tsx
@@ -1,30 +1,29 @@
-import babyNames from '../baby-names.json' 
+import babyNames from "../baby-names.json";
 // import namesBlock
 
-babyNames.sort(function(a, b) {
+babyNames.sort(function (a, b) {
   const textA = a.name.toUpperCase();
   const textB = b.name.toUpperCase();
-return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-})
+  return textA < textB ? -1 : textA > textB ? 1 : 0;
+});
 
-for (let i = 0; i < 10; i++) {    
-  console.log(babyNames[i].name)
+for (let i = 0; i < 10; i++) {
+  console.log(babyNames[i].name);
 }
 
-
-
-function NamesBlock(names: string[]): JSX.Element{
-    const blockStyle={backgroundColor:'lightblue',alignItems:'center',
-    marginTop:'5px'
-    }
-    return(
-        <>
-            
-            <div style={blockStyle}>HEllo</div>
-            <div style={blockStyle}>Hi</div>
-            <div style={blockStyle}>Hi again</div>
-        </>
-    )
+function NamesBlock(names: string[]): JSX.Element {
+  const blockStyle = {
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    marginTop: "5px",
+  };
+  return (
+    <>
+      <div style={blockStyle}>HEllo</div>
+      <div style={blockStyle}>Hi</div>
+      <div style={blockStyle}>Hi again</div>
+    </>
+  );
 }
 
-export default NamesBlock
+export default NamesBlock;
